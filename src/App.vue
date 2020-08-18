1836548a36cc9d3164a2b7c4d9de7e4f8961cc4b
zzh <260094892@qq.com>
2020-08-18 14:33:29 +0800

    解决tabbar无法正常工作的问题

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-08-09 13:47:05
- * @LastEditTime: 2020-08-12 23:14:33
+ * @LastEditTime: 2020-08-18 14:29:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue_cni\src\App.vue
@@ -19,21 +19,21 @@
 
     <!-- 底部 Tabbar 区域 -->
     <nav class="mui-bar mui-bar-tab">
-      <router-link class="mui-tab-item" to="/home">
+      <router-link class="mui-tab-item-llb" to="/home">
         <span class="mui-icon mui-icon-home"></span>
         <span class="mui-tab-label">首页</span>
       </router-link>
-      <router-link class="mui-tab-item" to="/member">
+      <router-link class="mui-tab-item-llb" to="/member">
         <span class="mui-icon mui-icon-contact"></span>
         <span class="mui-tab-label">会员</span>
       </router-link>
-      <router-link class="mui-tab-item" to="/shopcar">
+      <router-link class="mui-tab-item-llb" to="/shopcar">
         <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
           <span class="mui-badge">0</span>
         </span>
         <span class="mui-tab-label">购物车</span>
       </router-link>
-      <router-link class="mui-tab-item" to="/search">
+      <router-link class="mui-tab-item-llb" to="/search">
         <span class="mui-icon mui-icon-search"></span>
         <span class="mui-tab-label">搜索</span>
       </router-link>
@@ -67,6 +67,38 @@
 .v-enter-active,
 .v-leave-active {
   transition: all 0.5s ease;
+}
+
+// 该类名，解决 tabbar 点击无法切换的问题
+.mui-bar-tab .mui-tab-item-llb.mui-active {
+  color: #007aff;
+}
+
+.mui-bar-tab .mui-tab-item-llb {
+  display: table-cell;
+  overflow: hidden;
+  width: 1%;
+  height: 50px;
+  text-align: center;
+  vertical-align: middle;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: #929292;
+}
+
+.mui-bar-tab .mui-tab-item-llb .mui-icon {
+  top: 3px;
+  width: 24px;
+  height: 24px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+
+.mui-bar-tab .mui-tab-item-llb .mui-icon ~ .mui-tab-label {
+  font-size: 11px;
+  display: block;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
 
