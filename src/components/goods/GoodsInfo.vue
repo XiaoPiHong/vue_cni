90a94731d7b807d8bb386f0f74963598dc2ec380
zzh <260094892@qq.com>
2020-08-22 15:13:17 +0800

    vue-router中的编程式导航

---
+++ src/components/goods/GoodsInfo.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-08-22 12:04:19
+ * @LastEditTime: 2020-08-22 12:05:44
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue_cni\src\components\goods\GoodsInfo.vue
+-->
+<template>
+  <div>
+    <h3>这是商品详情页面</h3>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-09 13:47:05
- * @LastEditTime: 2020-08-20 09:46:34
+ * @LastEditTime: 2020-08-22 12:42:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue_cni\src\router.js
@@ -21,7 +21,7 @@ import NewsInfo from './components/news/NewsInfo.vue';
 import PhotoList from './components/photos/PhotoList.vue';
 import PhotoInfo from './components/photos/PhotoInfo.vue';
 import GoodsList from './components/goods/GoodsList.vue';
-
+import GoodsInfo from './components/goods/GoodsInfo.vue'
 
 //创建路由对象
 var router = new VueRouter({
@@ -64,7 +64,16 @@ var router = new VueRouter({
         {
             path: '/home/goodslist',
             component: GoodsList
-        }
+        },
+        /* {
+            path: '/home/goodsinfo/:id',
+            component: GoodsInfo
+        } */
+        {
+            path: '/home/goodsinfo/:id',
+            component: GoodsInfo,
+            name: 'goodsinfo'
+        },
     ],
     linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
 });
