3b43440ec882de633c9a75a6c4babb7e4d6fd357
zzh <260094892@qq.com>
2020-08-24 16:36:30 +0800

    绘制购物车页面中的商品列表的布局

---
+++ src/components/subcomponents/shopcar_numbox.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-08-22 22:45:59
+ * @LastEditTime: 2020-08-24 15:45:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue_cni\src\components\subcomponents\goodsinfo_numbox.vue
+-->
+<template>
+  <!-- 问题： 我们不知道什么时候能够拿到 max 值，但是，总归有一刻，会得到一个真正的 max 值 -->
+  <!-- 我们可以 使用 watch 属性监听，来 监听 父组件传递过来的 max 值，不管 watch 会被触发几次，但是，最后一次，肯定是一个 合法的 max 数值 -->
+  <div class="mui-numbox" data-numbox-min="1" style="height:25px;">
+    <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+    <input
+      id="test"
+      class="mui-input-numbox"
+      type="number"
+      value="1"
+      @change="countChanged"
+      ref="numbox"
+    />
+    <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+  </div>
+</template>
+<script>
+import mui from "../../lib/mui/js/mui.min.js";
+
+export default {
+  mounted() {
+    //初始化数字选择框组件
+    mui(".mui-numbox").numbox();
+  },
+  methods: {
+    countChanged() {
+      //数量改变了
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
--- src/components/tabbar/ShopcarContainer.vue
+++ src/components/tabbar/ShopcarContainer.vue
@@ -1,19 +1,85 @@
 <!--
  * @Author: your name
  * @Date: 2020-08-09 16:22:07
- * @LastEditTime: 2020-08-09 16:23:45
+ * @LastEditTime: 2020-08-24 15:56:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue_cni\src\components\tabbar\ShopcarContainer.vue
 -->
 <template>
-  <div>
-    <h3>ShopcarContainer</h3>
+  <div class="shopcar-container">
+    <div class="goods-list">
+      <!-- 商品列表项区域 -->
+      <div class="mui-card">
+        <div class="mui-card-content">
+          <div class="mui-card-content-inner">
+            <mt-switch></mt-switch>
+            <img
+              src="https://img.alicdn.com/imgextra/i4/2201702082835/O1CN01mg1Xl81WoTSE2MfOQ_!!2201702082835.jpg_430x430q90.jpg"
+            />
+            <div class="info">
+              <h1>4G全网通学生价超长待机安卓八核128G游戏智能手机</h1>
+              <p>
+                <span class="price">￥2199</span>
+                <numbox></numbox>
+                <a href="javascript:;">删除</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 结算区域 -->
+      <div class="mui-card">
+        <div class="mui-card-content">
+          <div
+            class="mui-card-content-inner"
+          >这是一个最简单的卡片视图控件；卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、点赞数量等</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import numbox from "../subcomponents/shopcar_numbox.vue";
+
+export default {
+  components: {
+    numbox,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.shopcar-container {
+  background-color: #eee;
+  overflow: hidden;
+
+  .goods-list {
+    .mui-card-content-inner {
+      display: flex;
+      align-items: center;
+    }
+
+    img {
+      width: 60px;
+      height: 60px;
+      margin-left: 5px;
+    }
+    h1 {
+      font-size: 13px;
+    }
+
+    .info {
+      padding-left: 5px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .price {
+        color: red;
+        font-weight: bold;
+      }
+    }
+  }
+}
 </style>
